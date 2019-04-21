28b45ea021fcac586c10fccbc200338a69ea871a
Pranali Kambli <pranalikambli2013@gmail.com>
2019-04-21 13:32:14 +0530

    original wordpress added

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,20 +20,20 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define( 'DB_NAME', 'database_name_here' );
 
-// ** Heroku Postgres settings - from Heroku Environment ** //
-$db = parse_url($_ENV["DATABASE_URL"]);
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', trim($db["path"],"/"));
 /** MySQL database username */
-define('DB_USER', $db["user"]);
+define( 'DB_USER', 'username_here' );
+
 /** MySQL database password */
-define('DB_PASSWORD', $db["pass"]);
+define( 'DB_PASSWORD', 'password_here' );
+
 /** MySQL hostname */
-define('DB_HOST', $db["host"]);
+define( 'DB_HOST', 'localhost' );
+
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
+
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
